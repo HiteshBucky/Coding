d3dfd903ee93e70fb218d3b7a791dc7803e504bb
Your NameHitesh <hk824778@gmail.com>
2020-07-30 15:38:34 +0530

    Optimizedd https://leetcode.com/problems/minimum-subsequence-in-non-increasing-order/

--- Solution.java
+++ Solution.java
@@ -1,42 +1,29 @@
-/*
-
-Given the array nums, obtain a subsequence of the array whose sum of elements is strictly greater than the sum of the non included elements in such subsequence. 
-
-If there are multiple solutions, return the subsequence with minimum size and if there still exist multiple solutions, return the subsequence with the maximum total sum of all its elements. A subsequence of an array can be obtained by erasing some (possibly zero) elements from the array. 
-
-Note that the solution with the given constraints is guaranteed to be unique. Also return the answer sorted in non-increasing order.
-
- 
-
-Example 1:
-
-Input: nums = [4,3,10,9,8]
-Output: [10,9] 
-Explanation: The subsequences [10,9] and [10,8] are minimal such that the sum of their elements is strictly greater than the sum of elements not included, however, the subsequence [10,9] has the maximum total sum of its elements. 
-
-
-*/
-
 import java.util.*;
 import java.io.*;
 
 
 class Solution{
-	public static List<Integer> solve(int arr[]) {
-		int size = arr.length;
-		List<Integer> list = new ArrayList<>();
-
-		Arrays.sort(arr);
-
-		int sum = Arrays.stream(arr).sum();
-
-		int listSum = 0;
-		for(int i = size-1; listSum<=sum; i--){
-			list.add(arr[i]);
-			listSum += arr[i];
-			sum -= arr[i];
-		}
-		return list;
+	public static List<Integer> solve(int nums[]) {
+		 List<Integer> list = new ArrayList<>();
+		int[] count = new int[101];
+        
+	    int totalSum = 0;
+	    for (int currInt : nums) {
+	        totalSum += currInt;
+	        count[currInt]++;
+	    }
+	    
+	    int currSum = 0;
+	    for (int i = count.length - 1; currSum<=totalSum && i >= 0; --i) {
+	        while (count[i] > 0 && currSum<=totalSum) {
+	            list.add(i);
+	            currSum += i;
+	            totalSum -= i;
+	            count[i]--;
+	        }
+	    }
+	    
+	    return list;
 	}
 	
 	public static void main(String[] args)  throws IOException {
