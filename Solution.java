0bedb40c84f8faf5d227206c0e187903be7b7b1a
Your NameHitesh <hk824778@gmail.com>
2020-07-30 16:03:13 +0530

    Water Bottles https://leetcode.com/problems/water-bottles/

--- Solution.java
+++ Solution.java
@@ -3,38 +3,21 @@ import java.io.*;
 
 
 class Solution{
-	public static List<Integer> solve(int nums[]) {
-		 List<Integer> list = new ArrayList<>();
-		int[] count = new int[101];
-        
-	    int totalSum = 0;
-	    for (int currInt : nums) {
-	        totalSum += currInt;
-	        count[currInt]++;
-	    }
-	    
-	    int currSum = 0;
-	    for (int i = count.length - 1; currSum<=totalSum && i >= 0; --i) {
-	        while (count[i] > 0 && currSum<=totalSum) {
-	            list.add(i);
-	            currSum += i;
-	            totalSum -= i;
-	            count[i]--;
-	        }
-	    }
-	    
-	    return list;
+	public static int solve(int sum, int x) {
+		int ans = sum;
+		int temp;
+		while(sum>=x){
+			temp = sum/x;
+			ans += temp;
+			sum = (sum - x*temp) + temp;
+		}
+		return ans;
 	}
 	
 	public static void main(String[] args)  throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int size = sc.nextInt();
-		int arr[] = new int[size];
-
-		for(int i=0; i<size; i++)
-			arr[i] = sc.nextInt();
-
-		List<Integer> list = solve(arr);
-		System.out.println(list);
+		int sum = sc.nextInt();
+		int x = sc.nextInt();
+		System.out.println(solve(sum, x));
 	}
 }
